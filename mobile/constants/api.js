79052fdad72dc27e4129ca3291c91c5be99e7056
Rinali Pesakya <rinalipesakya@gmail.com>
2026-07-10 09:50:13 +0530

    Add Wellness Timeline for My Journey page to track what assessments user has completed and not

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,26 +1,27 @@
 import Constants from "expo-constants";
 import { Platform } from "react-native";
 
+// function resolveApiHost() {
+//   const explicitUrl = process.env.EXPO_PUBLIC_API_URL;
+//   if (explicitUrl) return explicitUrl.replace(/\/$/, "");
+
+//   const hostUri = Constants.expoConfig?.hostUri;
+//   if (hostUri) {
+//     const hostMatch = hostUri.match(/^(?:[a-z]+:\/\/)?([^:/]+)/i);
+//     if (hostMatch?.[1]) return `http://${hostMatch[1]}:3000/api`;
+//   }
+
+//   const fallback = Platform.OS === "android" ? "10.0.2.2" : "localhost";
+//   return `http://${fallback}:3000/api`;
+// }
 function resolveApiHost() {
   const explicitUrl = process.env.EXPO_PUBLIC_API_URL;
   if (explicitUrl) return explicitUrl.replace(/\/$/, "");
 
-  const hostUri = Constants.expoConfig?.hostUri;
-  if (hostUri) {
-    const hostMatch = hostUri.match(/^(?:[a-z]+:\/\/)?([^:/]+)/i);
-    if (hostMatch?.[1]) return `http://${hostMatch[1]}:3000/api`;
-  }
-
-  const fallback = Platform.OS === "android" ? "10.0.2.2" : "localhost";
-  return `http://${fallback}:3000/api`;
+  // Update this to use your exact live centralindia URL
+  return "https://carewave-backend-caapeae6hecqcbbw.centralindia-01.azurewebsites.net/api";
 }
-// function resolveApiHost() {
-//   const explicitUrl = process.env.EXPO_PUBLIC_API_URL;
-//   if (explicitUrl) return explicitUrl.replace(/\/$/, "");
-// 
-//   // Update this to use your exact live centralindia URL
-//   return "https://carewave-backend-caapeae6hecqcbbw.centralindia-01.azurewebsites.net/api";
-// }
+
 export const API_URL = resolveApiHost();
 
 export async function fetchWithTimeout(url, options = {}, timeoutMs = 10000) {
